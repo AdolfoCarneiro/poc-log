aa05081aab7f2595feaeb94ac58946da429e1833
adolfo carneiro <adolfocarneiro555@gmail.com>
2025-03-25 23:55:33 -0300

    feat: adicionando add logging

--- poc-log/Program.cs
+++ poc-log/Program.cs
@@ -23,6 +23,16 @@ namespace poc_log
 
             builder.Services.AddApplicationInsightsTelemetry();
             builder.Logging.AddApplicationInsights();
+            builder.Logging.AddConsole();
+            builder.Services.AddLogging(logBuilder =>
+            {
+                logBuilder.AddConsole();
+                logBuilder.SetMinimumLevel(LogLevel.Information);
+                logBuilder.AddApplicationInsights(
+                    configureTelemetryConfiguration: (config) => config.ConnectionString = "InstrumentationKey=baedcb7c-18c2-4e7d-9c6a-62e12bbad35f;IngestionEndpoint=https://canadacentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://canadacentral.livediagnostics.monitor.azure.com/;ApplicationId=c10c057c-f7ee-4f00-b30e-672467ef84f7",
+                    configureApplicationInsightsLoggerOptions: (options) => { }
+                    );
+            });
 
             var app = builder.Build();
 
@@ -30,7 +40,7 @@ namespace poc_log
             app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
-
+            //app.UseRouting();
             app.UseAuthorization();
 
 
