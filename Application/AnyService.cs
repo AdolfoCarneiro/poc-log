6dcf96c8d2879d1786064cbd14f775a088e43f7f
Adolfo Carneiro <adolfo.carneiro@gowsolucoes.com>
2025-03-26 10:42:46 -0300

    feat: adicionando log de tudo que e tipo e configuracao do appsettings

--- Application/AnyService.cs
+++ Application/AnyService.cs
@@ -11,7 +11,13 @@ namespace Application
 
         public int DoAnithing()
         {
-            _logger.LogInformation("Chamando DoAnithing");
+            _logger.LogInformation("Chamando DoAnithing info");
+            _logger.LogWarning("Chamando DoAnithing warn");
+            _logger.LogError("Chamando DoAnithing error");
+            _logger.LogCritical("Chamando DoAnithing critical");
+            _logger.LogDebug("Chamando DoAnithing debug");
+            _logger.LogTrace("Chamando DoAnithing trace");
+
             var a = 1;
             var b = 2;
             var c = a + b;
--- poc-log/Program.cs
+++ poc-log/Program.cs
@@ -3,7 +3,6 @@ using Application;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace poc_log
 {
@@ -13,24 +12,21 @@ namespace poc_log
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddScoped<IAnyService, AnyService>();
-            builder.Services.AddApplicationInsightsTelemetry();
-            builder.Services.AddLogging(builder => builder.AddApplicationInsights());
-            builder.Logging.AddApplicationInsights() ;
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddScoped<IAnyService, AnyService>();
+
+            builder.Services.AddApplicationInsightsTelemetry();
+            builder.Logging.AddApplicationInsights();
+
             var app = builder.Build();
 
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            
             app.UseHttpsRedirection();
             app.UseAuthorization();
 
